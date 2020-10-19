1949dea4c320e2c6bea393a047215934199406aa
Tony Ivchenko <toxa.ivchenko@gmail.com>
2020-10-19 15:53:05 -0800

    build script: make pybullet symlink setup idempotent

--- build_cmake_pybullet_double.sh
+++ build_cmake_pybullet_double.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
 
+set -eu
+
 if [ -e CMakeCache.txt ]; then
-  rm CMakeCache.txt
+  rm -f CMakeCache.txt
 fi
 mkdir -p build_cmake
 cd build_cmake
-cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=ON -DUSE_DOUBLE_PRECISION=ON -DBT_USE_EGL=ON -DCMAKE_BUILD_TYPE=Release .. || exit 1
-make -j $(command nproc 2>/dev/null || echo 12) || exit 1
-cd examples
-cd pybullet
+cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=ON -DUSE_DOUBLE_PRECISION=ON -DBT_USE_EGL=ON -DCMAKE_BUILD_TYPE=Release ..
+make -j "$(nproc 2>/dev/null || echo 12)"
+cd examples/pybullet
 if [ -e pybullet.dylib ]; then
-  ln -f -s pybullet.dylib pybullet.so
+  ln -sf pybullet.dylib pybullet.so
 fi
-if [ -e pybullet_envs ]; then
-  rm pybullet_envs
-fi
-if [ -e pybullet_data ]; then
-  rm pybullet_data
-fi
-if [ -e pybullet_utils ]; then
-  rm pybullet_utils
-fi
+rm -rf pybullet_envs pybullet_data pybullet_utils
 ln -s ../../../examples/pybullet/gym/pybullet_envs .
 ln -s ../../../examples/pybullet/gym/pybullet_data .
 ln -s ../../../examples/pybullet/gym/pybullet_utils .
